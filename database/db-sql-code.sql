b92c1a6d6aa3884785187c38b575e1af31b1eb33
lucasmercau <lucas.mercau99@gmail.com>
2024-03-10 01:09:12 -0300

    owner name change

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -2,7 +2,7 @@ CREATE TYPE public.account_type AS ENUM
     ('Client', 'Employee', 'Admin');
 
 ALTER TYPE public.account_type
-    OWNER TO cse340lm;
+    OWNER TO cse340w2;
 
 -- Table structure for table `classification`
 CREATE TABLE public.classification (
