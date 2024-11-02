a99aee5242ee7f4e38a8453c9b01e6c62ba6b4a8
lucasmercau <lucas.mercau99@gmail.com>
2024-11-02 17:53:53 -0300

    databse update

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -2,7 +2,7 @@ CREATE TYPE public.account_type AS ENUM
     ('Client', 'Employee', 'Admin');
 
 ALTER TYPE public.account_type
-    OWNER TO cse340w2;
+    OWNER TO cse340w9;
 
 -- Table structure for table `classification`
 CREATE TABLE public.classification (
